bbcc8b5694ee46089962d05124f08f414ee72821
aminotto <alexis.minotto@gmail.com>
2017-03-09 11:14:50 +0100

    maj du css 1

--- index.php
+++ index.php
@@ -13,27 +13,27 @@
 	</div>
     <div class="divlogo">
 		<input type="image" src="img/cursorLogo.png" id="point" class="logo" width="40">
-		<span class="popup">Placer des points</span>
+		<div class="popup"><p>Placer des points</p></div>
 	</div>
 	<div class="divlogo">
 		<input type="image" src="img/segmentLogo.png" id="line" class="logo" width="40">
-		<span class="popup">Tracer des segments</span>
+		<div class="popup"><p>Tracer des segments</p></div>
 	</div>
 	<div class="divlogo">
 		<input type="image" src="img/loupeLogo.png" id="loupe" class="logo" width="40">
-		<span class="popup">Zoomer</span>
+		<div class="popup"><p>Zoomer</p></div>
 	</div>
 	<div class="divlogo">
 		<input type="image" src="img/cercleLogo.png" id="cercle" class="logo" width="40">
-		<span class="popup">Tracer un cercle</span>
+		<div class="popup"><p>Tracer un cercle</p></div>
 	</div>
     <div class="divlogo">
 		<input type="image" src="img/dropLogo.png" id="dragAndDrop" class="logo" width="40">
-		<span class="popup">Drag and drop</span>
+		<div class="popup"><p>Drag and drop</p></div>
 	</div>
 	<div class="divlogo">
 		<input type="image" src="img/rotationLogo.png" id="rotation" class="logo" width="40">
-		<span class="popup">Rotation</span>
+		<div class="popup"><p>Rotation</p></div>
 	</div>
 </div>
 <div id="canvasContainer">
